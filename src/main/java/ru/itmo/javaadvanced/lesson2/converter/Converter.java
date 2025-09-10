d439d784a3c9e329b6daaa27fd77fd6dc1767342
Karpilenko Roman <karpilenko.roman@rwb.ru>
2025-09-10 20:56:30 +0300

    Did lesson #2
    (cherry picked from commit f3cf63915398e0dc498005196041bcce24ca0333)

---
+++ src/main/java/ru/itmo/javaadvanced/lesson2/converter/Converter.java
@@ -0,0 +1,6 @@
+package ru.itmo.javaadvanced.lesson2.converter;
+
+
+public interface Converter<I, O> {
+    O convert(I input);
+}
---
+++ src/main/java/ru/itmo/javaadvanced/lesson2/model/Month.java
@@ -0,0 +1,16 @@
+package ru.itmo.javaadvanced.lesson2.model;
+
+public enum Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
