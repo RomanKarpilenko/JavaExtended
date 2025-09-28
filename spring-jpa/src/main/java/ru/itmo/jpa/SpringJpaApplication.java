ec4fd654814ae9f41f10386ddaadcd95858f6674
Karpilenko Roman <karpilenko.roman@rwb.ru>
2025-09-28 11:48:46 +0300

    Added liquibase

--- spring-jpa/src/main/java/ru/itmo/jpa/SpringJpaApplication.java
+++ spring-jpa/src/main/java/ru/itmo/jpa/SpringJpaApplication.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import ru.itmo.jpa.repository.CityRepository;
-import ru.itmo.jpa.repository.TranslationKeyRepository;
-import ru.itmo.jpa.repository.TranslationRepository;
 import ru.itmo.jpa.util.State;
 import ru.itmo.jpa.util.StateMachine;
 
@@ -16,14 +13,7 @@ public class SpringJpaApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringJpaApplication.class, args);
-        CityRepository cityRepository = applicationContext.getBean(CityRepository.class);
-        TranslationRepository translationRepository = applicationContext.getBean(TranslationRepository.class);
-        TranslationKeyRepository translationKeyRepository = applicationContext.getBean(TranslationKeyRepository.class);
         StateMachine stateMachine = applicationContext.getBean(StateMachine.class);
-
-        cityRepository.deleteAll();
-        translationRepository.deleteAll();
-        translationKeyRepository.deleteAll();
 
         while (stateMachine.getCurrentState() != State.EXIT) {
             stateMachine.start();
